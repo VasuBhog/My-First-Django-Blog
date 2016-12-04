2c0380e721a7bc058aca01c2202d404e9eb7c005
Vdog12 <bhogvu@mail.uc.edu>
2016-12-04 03:00:00 -0500

    Additional files from security to comment module and NOW Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,21 @@ STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+#Login is accessed directl
+LOGIN_REDIRECT_URL = '/'
+
+#heroku deployment
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
